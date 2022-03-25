d877f17bbb0ae34c165c04ad04bd19f1daa0f389
bill-ncc1631 <bill.ncc1631@hotmail.com>
2022-03-25 18:11:40 -0300

    del

--- vpn-dfa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo openconnect --protocol=gp vpn-dfa.jfsc.jus.br --useragent='PAN GlobalProtect'  --user=cev82 --no-passwd --form-entry main:Password=StarTr3k1701NCC --os=linux-64 --no-xmlpost --verbose --dump-http-traffic;
-ls;
